a58500d625ea8356e5292efc685eb59fb4beffc4
weitianyi <weitianyius1996@163.com>
2021-08-29 18:58:47 -0400

    add maximum pairwise product

--- assignments/week1_programming_challenges/2_maximum_pairwise_product.py
+++ assignments/week1_programming_challenges/2_maximum_pairwise_product.py
@@ -13,6 +13,19 @@ print(numbers[-1]*numbers[-2])
 #             max_result = max(numbers[i]*numbers[j], max_result)
 #     return max_result
 
+# scan twices
+def max_pairwise_product(numbers):
+    max_elm = sec_elm = 0
+    for i in range(len(numbers)):
+        max_elm = max(numbers[i], max_elm)
+    max_index = numbers.index(max_elm)
+    for j in range(len(numbers)):
+        if numbers[j] > sec_elm and j != max_index:
+            sec_elm = numbers[j]
+    return max_elm*sec_elm
+
+print(max_pairwise_product([1,2,3,5,5,2]))
+
 
 
 
