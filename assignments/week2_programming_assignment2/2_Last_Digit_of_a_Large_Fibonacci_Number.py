5ded4e44d4df96289ac66e03d2e5cbcfae388fd8
weitianyi <weitianyius1996@163.com>
2022-03-16 23:03:35 -0400

    add q2 and q4

--- assignments/week2_programming_assignment2/2_Last_Digit_of_a_Large_Fibonacci_Number.py
+++ assignments/week2_programming_assignment2/2_Last_Digit_of_a_Large_Fibonacci_Number.py
@@ -8,8 +8,8 @@ def get_fibonacci_last_digit_naive(n):
         res = [0]*(1+n)
         res[1] = 1
         for i in range(2, n+1):
-            res[i] = res[i-2]+res[i-1]
-        return res[-1] % 10
+            res[i] = (res[i-2]+res[i-1])%10
+        return res[-1]
 
 
 if __name__ == '__main__':
---
+++ assignments/week2_programming_assignment2/4_least_common_multiple.py
@@ -0,0 +1,31 @@
+# Uses python3
+import sys
+
+
+def lcm_naive(a, b):
+    gcd_acc = 1
+    common_gcd = gcd(a,b)
+    while common_gcd > 1:
+        gcd_acc = gcd_acc*common_gcd
+        a = a / common_gcd
+        b = b / common_gcd
+        common_gcd = gcd(a, b)
+    return gcd_acc*a*b
+
+def gcd(a,b):
+    if b == 0:
+        return a
+    elif a >= b:
+        b_rm = a % b
+        a = b
+        return gcd(a, b_rm)
+    else:
+        return gcd(b, a)
+
+# print(lcm_naive(6,8))
+
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    a, b = map(int, input.split())
+    print(lcm_naive(a, b))
