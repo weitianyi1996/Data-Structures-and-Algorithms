c94eb985038cc74feeeb5d9dd0fdadc17dd94446
weitianyi <weitianyius1996@163.com>
2022-03-22 23:23:34 -0400

    add fractional knapsack

---
+++ Algorithmic_Toolbox_assignments/week3_programming_assignment_greedy/2_fractional_knapsack.py
@@ -0,0 +1,30 @@
+# Uses python3
+import sys
+
+
+def get_optimal_value(capacity, weights, values):
+    value = 0.
+    curr_cap = 0
+    dic = {}
+    for i,v in enumerate(weights):
+        value_per_weight = values[i]/weights[i]
+        dic[value_per_weight] = weights[i]  # max is wight
+    sort_list = sorted(dic.keys(), reverse=True)
+    p = 0
+
+    while curr_cap < capacity:
+        load_cap_this_time = min(capacity-curr_cap, dic[sort_list[p]])
+        value += sort_list[p]*load_cap_this_time
+        curr_cap += load_cap_this_time
+        p += 1  # move to next valuable item
+
+    return value
+
+
+if __name__ == "__main__":
+    data = list(map(int, sys.stdin.read().split()))
+    n, capacity = data[0:2]
+    values = data[2:(2 * n + 2):2]  # 60
+    weights = data[3:(2 * n + 2):2]  # 20
+    opt_value = get_optimal_value(capacity, weights, values)
+    print("{:.10f}".format(opt_value))
