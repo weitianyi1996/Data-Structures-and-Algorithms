588a56e47116cac388c94c3ac54441ff9a5bec0a
weitianyi <weitianyius1996@163.com>
2022-06-14 23:14:00 -0500

    fix money change

--- Algorithmic_Toolbox_assignments/week5_programming_assignment_dynamic_programming/1_money_change_again.py
+++ Algorithmic_Toolbox_assignments/week5_programming_assignment_dynamic_programming/1_money_change_again.py
@@ -1,13 +1,18 @@
 # Uses python3
 import sys
 
+
 def get_change(m):
     # write your code here
-    res_arr = [0 for _ in range(m+1)]  # include 0 index, $0, need return 0 bill
-    res_arr[0], res_arr[1], res_arr[2], res_arr[3] = 0,1,2,1
-    if m <= 3:
-        return res_arr[m]
-    else:
+    if m == 1:
+        return 1
+    elif m == 2:
+        return 2
+    elif m == 3:
+        return 1
+    else:  # >3
+        res_arr = [0 for _ in range(m + 1)]  # include 0 index, $0, need return 0 bill
+        res_arr[0], res_arr[1], res_arr[2], res_arr[3] = 0, 1, 2, 1
         for i in range(4, m+1):
             res_arr[i] = min(res_arr[i-1]+1, res_arr[i-3]+1, res_arr[i-4]+1)
         return res_arr[m]
